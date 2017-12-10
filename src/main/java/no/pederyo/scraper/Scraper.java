e98d70ac0f38d394809dfd923ad000da5044a935
pederDesktop <peder.wiig@gmail.com>
2017-12-10 01:56:53 +0100

    Ordnet opp i iterasjoner og øking i verdi på 0.3 %

--- src/main/java/no/pederyo/scraper/Scraper.java
+++ src/main/java/no/pederyo/scraper/Scraper.java
@@ -11,40 +11,44 @@ import static no.pederyo.scraper.VerdiSjekker.sjekkforjeVerdi;
 import static no.pederyo.util.CoinUtil.*;
 
 public class Scraper {
+    private static final String MELDING_START = "IoTa-Tracking har nå startet. Current Verdi: ";
     private static double oldValue = 0.0;
     private static int iterasjon = 0;
-    private static final String MELDING_START = "IoTa-Tracking har nå startet. Current Verdi: ";
+
     public static void kjorProgram(Coin c) {
         oldValue = scrape();
         PushBullet.client.sendNotePush("TRACKING STARTET", MELDING_START + oldValue + " USD");
+        leggTilVerdi(oldValue, c);
         leggTilVerdi(oldValue, c);
         Avkastning avkastning = new Avkastning();
         regnUtSetTotalOgAvkastning(oldValue, c);
         avkastning.leggTilAvkastning(c); // Oppretter node.
         while (true) {
             try {
-                enScrapeIterasjon(c, avkastning);
                 Thread.sleep(10000);
+                enScrapeIterasjon(c, avkastning);
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
     }
-
-
     private static void enScrapeIterasjon(Coin coin, Avkastning avk) {
         iterasjon++;
         double verdi = scrape();
         regnUtSetTotalOgAvkastning(verdi, coin);
-        skrivTilLogg(verdi, coin);
         if (iterasjon % 3 == 0) { //lagrer gammel verdi vært 30 sek
             oldValue = verdi;
         }
+        if (iterasjon % 6 == 0) {
+            skrivTilLogg(verdi, coin); // skriver til log vært 60 sek
+        }
         if (iterasjon % 180 == 0) { //sjekker mot gammel verdi. Hver halv time.
-            sjekkValletPushNot(coin, verdi); //sjekker gammel mot current verdi.
-            avk.leggTilAvkastning(coin); // legger til nyAvkastning
-            sjekkAvkastning(avk); // sjekker avkastning.
-            leggTilVerdi(verdi, coin);
+            boolean leggTil = sjekkValletPushNot(coin, verdi); //sjekker gammel mot current verdi.
+            avk.leggTilAvkastning(coin);
+            sjekkAvkastning(avk);
+            if (leggTil) {
+                leggTilVerdi(verdi, coin); //legger til ny verdi om det har vært en økning på 0.3 siden forjegang.
+            }
         }
         sjekkforjeVerdi(oldValue, verdi);
         sjekkMilePeler(verdi);
@@ -55,7 +59,6 @@ public class Scraper {
 
     /**
      * Setter opp pb, logg og planlegger.
-     *
      * @param args
      * @param coin
      */
